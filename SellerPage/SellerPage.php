7dd11b216f57b39f9b6723a55c4140baa3d6599f
nathanfekade <nathanfekade276@gmail.com>
2024-02-03 11:12:06 +0300

    updated seller page so that seller can input form

--- SellerPage/SellerPage.php
+++ SellerPage/SellerPage.php
@@ -1,3 +1,54 @@
+<?php
+
+include('../Classes/Connect.php');
+include('../Classes/Product.php');
+
+$ProductName = $ProductCategory=$ProductImageFileName = $ProductBasePrice=$ProductDescription =null;
+
+ // target directory
+ $targetDir="../productImageUploads/";  
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST"&& isset($_FILES['ProductImageFileName']) )
+    
+     {
+  
+      // && isset($_POST["ProductName"])&&isset($_POST  ["ProductCategory"])&&isset($_POST["ProductImageFileName"])&&isset($_POST["ProductBasePrice"])&&isset($_POST["ProductDescription"])
+    
+      $fileName=basename($_FILES["ProductImageFileName"]["name"]);
+     
+      $targetPath=$targetDir.$fileName;
+
+      $ProductName = test_input($_POST["ProductName"]);
+      $ProductCategory = test_input($_POST["ProductCategory"]);
+      $ProductImageFileName =$targetPath;
+      $ProductBasePrice = test_input($_POST["ProductBasePrice"]);
+      $ProductDescription = test_input($_POST["ProductDescription"]);
+    
+      if(move_uploaded_file($_FILES["ProductImageFileName"]["tmp_name"],$targetPath)){
+
+  $productInstance = new Product();
+   $productInstance->addProduct($ProductName,
+   $ProductCategory,$ProductImageFileName, $ProductBasePrice,
+   $ProductDescription);
+      }
+      else{
+        echo "Error Moving the file";
+      }
+      
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);//check what it does
+  $data = htmlspecialchars($data);
+  return $data;
+}
+
+
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,6 +57,38 @@
   <title>Document</title>
 </head>
 <body>
-  <h1> seller page</h1>
+
+      <div class="wrapper">
+      <form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" enctype="multipart/form-data">
+
+
+      
+      
+            <div class="input-box">
+              <input type="text" name="ProductName" id="ProductName" required placeholder="ProductName"> 
+            </div>
+
+            <div class="input-box">
+              <input type="text" name="ProductCategory" id="ProductCategory" required placeholder="ProductCategory"> 
+            </div>
+
+            
+            <div class="input-box">
+              <input type="file" name="ProductImageFileName" id="ProductImageFileName" required placeholder="ProductImage"> 
+            </div>
+           
+            <div class="input-box">
+              <input type="text" name="ProductBasePrice" id="ProductBasePrice" required placeholder="ProductBasePrice"> 
+            </div>
+
+            <div class="input-box">
+              <input type="text" name="ProductDescription" id="ProductDescription" required placeholder="ProductDescription"> 
+            </div>
+
+            <input type="submit" value="Insert" name="Insert"class="Insert-btn"> <br>
+
+            </form>
+
+            </div>
 </body>
 </html>
