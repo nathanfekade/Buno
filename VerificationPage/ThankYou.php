34f04c5a4fbddf8e57906140fbd6c86215a933e2
nathanfekade <nathanfekade276@gmail.com>
2024-02-07 14:55:03 +0300

    created thank youpage

---
+++ VerificationPage/ThankYou.php
@@ -0,0 +1,15 @@
+<?php
+header("Refresh:5;url=../HomePage/HomePage.php");
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Thank You</title>
+</head>
+<body>
+  <h1>Thank you for registering we will verify your account in a few days</h1>
+</body>
+</html>
