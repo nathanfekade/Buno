7b991ab38d5452f308a0beb531c589d31f4810c1
nathanfekade <nathanfekade276@gmail.com>
2024-02-09 19:18:43 +0300

    updated chat page

--- chat/BuyerChat.php
+++ chat/BuyerChat.php
@@ -9,50 +9,50 @@ $db=$Con->getConnection();
 $buyerUserGlobal=$_SESSION['BuyerUserName'] ;
 
 $sellerUserGlobal=$_SESSION["SellerProductUserName"];
+echo $sellerUserGlobal;
 
-function listTexts($sellerUser){
+function listTexts($sellerUserGlobal){
 
   global $buyerUserGlobal;
   global $db;
-echo "sel $sellerUser";
-echo "<br>";
-echo "buy $buyerUserGlobal";
-echo "<br>";
+  echo '<div class="header">
+        <div class="receiverInfo">
+        <span class="material-symbols-outlined">account_circle</span>
+        <span class="receiverUserName">'.$sellerUserGlobal.'</span>
+        </div>
+        <span class="material-symbols-outlined">arrow_back</span>
+        </div>
+        <div class="chatBox">';
 
-  $sql2 = "SELECT sellerConvo,buyerConvo FROM chat where BuyerUserName='$buyerUserGlobal' and SellerUserName='$sellerUser' ORDER by chatTime";
+  $sql2 = "SELECT sellerConvo,buyerConvo FROM chat where BuyerUserName='$buyerUserGlobal' and SellerUserName='$sellerUserGlobal' ORDER by chatTime";
   $res2=$db->query($sql2);  
   if($res2->num_rows>0)
   {
 
     while($row=$res2->fetch_assoc())
     {
-      echo "<br>";
       if($row['sellerConvo']!=null){
-        // echo "<div class='sellerChat'>seller:  $row['sellerConvo']</div>";
-        echo "<div class='sellerChat'>seller: ".$row['sellerConvo']."</div>";
-
-
-        // echo "seller:  ".$row['sellerConvo'];
-        echo "<br>";
+        echo "<div class='receiverChat'><span>".$row['sellerConvo']."</span></div>";
       }
       if($row['buyerConvo']!=null){
-        // echo "<div class='buyerChat'>buyer:  $row['buyerConvo']</div>";
-        echo "<div class='buyerChat'>buyer: ".$row['buyerConvo']."</div>";
-
-        // echo "buyer: ". $row['buyerConvo'];
-      echo "<br>";
-      }
+        echo "<div class='senderChat'><span>".$row['buyerConvo']."</span></div>";
     }
   }
+}
+echo "</div>";
 }
 function defaultText(){
 
   global $buyerUserGlobal;
   global $sellerUserGlobal;
-  //echo "sel $sellerUserGlobal";
-  //echo "<br>";
-  //echo "buy $buyerUserGlobal";
- // echo "<br>";
+ echo '<div class="header">
+      <div class="receiverInfo">
+      <span class="material-symbols-outlined">account_circle</span>
+      <span class="receiverUserName">'.$sellerUserGlobal.'</span>
+      </div>
+      <span class="material-symbols-outlined">arrow_back</span>
+      </div>
+      <div class="chatBox">';
   global $db;
   $sql2 = "SELECT sellerConvo,buyerConvo FROM chat where BuyerUserName='$buyerUserGlobal' and SellerUserName='$sellerUserGlobal' ORDER by chatTime";
   $res2=$db->query($sql2);  
@@ -61,43 +61,20 @@ function defaultText(){
 
     while($row=$res2->fetch_assoc())
     {
-      echo "<br>";
       if($row['sellerConvo']!=null){
-        // echo "<div class='sellerChat'>seller:  $row['sellerConvo']</div>";
-        echo "<div class='sellerChat'>seller: ".$row['sellerConvo']."</div>";
-
-
-        // echo "seller:  ".$row['sellerConvo'];
-        echo "<br>";
+        echo "<div class='receiverChat'><span>".$row['sellerConvo']."</span></div>";
       }
       if($row['buyerConvo']!=null){
-        // echo "<div class='buyerChat'>buyer:  $row['buyerConvo']</div>";
-        echo "<div class='buyerChat'>buyer: ".$row['buyerConvo']."</div>";
-
-        // echo "buyer: ". $row['buyerConvo'];
-      echo "<br>";
-      }
+        echo "<div class='senderChat'><span>".$row['buyerConvo']."</span></div>";
     }
   }
 }
-
-function send($chatValue,$sellerUser){
-  global $db;
-  
-  global $buyerUserGlobal;
-  
-  $textInsertTime = getdate();
-  // echo "buyer username= $buyerUsername || ";
-  $sql = "INSERT INTO chat ( SellerUserName, BuyerUserName, buyerConvo, sellerConvo,chatTime) VALUES ('$sellerUser','$buyerUserGlobal' , '$chatValue', NULL,'$textInsertTime[0]')";
-
-  
-$res =  $db->query($sql);
-
-// listTexts($sellerUser,$buyerUser);
+echo "</div>";
 }
-function sendDefault($chatValue){
+
+function send($chatValue,$sellerUserGlobal){
   global $db;
-  global $sellerUserGlobal;
+  
   global $buyerUserGlobal;
   
   $textInsertTime = getdate();
@@ -109,50 +86,20 @@ $res =  $db->query($sql);
 
 // listTexts($sellerUser,$buyerUser);
 }
-// echo $SellerUserName2;
+// function sendDefault($chatValue){
+//   global $db;
+//   global $sellerUserGlobal;
+//   global $buyerUserGlobal;
+  
+//   $textInsertTime = getdate();
+//   // echo "buyer username= $buyerUsername || ";
+//   $sql = "INSERT INTO chat ( SellerUserName, BuyerUserName, buyerConvo, sellerConvo,chatTime) VALUES ('$sellerUserGlobal','$buyerUserGlobal' , '$chatValue', NULL,'$textInsertTime[0]')";
 
-// echo 'sel: '.$sellerUser;
-//     echo "<br>";
-//     echo 'buy: '. $buyerUserGlobal;
-//     ;
-//     echo "<br>";
+  
+// $res =  $db->query($sql);
 
-// if ($_SERVER["REQUEST_METHOD"] == "POST") {
-//   if (isset($_POST['submitUserName'])) {
-//     $sellerUser = $_POST['value'];
-//     $buyerUser=$_SESSION['BuyerUserName'] ;
-//     echo 'inside post sel: '.$sellerUser;
-//     echo "<br>";
-//     echo 'inside post buy: '.$buyerUser;
-//     // listTexts($sellerUser,$buyerUser);
-//     // $sql2 = "SELECT sellerConvo,buyerConvo FROM chat where BuyerUserName='$buyerUser' and SellerUserName='$sellerUser' ORDER by chatTime";
-    
-
-//     // $res2=$db->query($sql2);  
-//     // if($res2->num_rows>0)
-//     // {
-
-//     //   while($row=$res2->fetch_assoc())
-//     //   {
-//     //     echo "<br>";
-//     //     if($row['sellerConvo']!=null){
-//     //       echo "seller: ".$row['sellerConvo'];
-//     //       echo "<br>";
-//     //     }
-//     //     if($row['buyerConvo']!=null){
-//     //       echo "buyer: ". $row['buyerConvo'];
-//     //     echo "<br>";
-//     //     }
-//     //   }
-//     // }
-//   }
+// // listTexts($sellerUser,$buyerUser);
 // }
-
-
-
-
-
-
 
 
 function test_input($data) {
@@ -169,7 +116,13 @@ function test_input($data) {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="BuyerChat.css" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link href="https://fonts.googleapis.com/css2?family=Exo+2:ital,wght@0,100..900;1,100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+  <link rel="stylesheet" href="BuyerChat.css?v=<?php echo time(); ?>" />
  
   <title>BuyerChat</title>
   
@@ -193,17 +146,21 @@ function test_input($data) {
           {
       
             array_push($sellers, $row['SellerUserName']);
+            echo $row['SellerUserName'];
         
           }
     }
         
     $uniqueSeller = array_unique($sellers);
     foreach ($uniqueSeller as $x) {
+      echo $x;
       // echo "<div class=$x>$x</div>";
       echo " <div class='chatListElement' id='$x'>
       <form action='' method='post'>
         <input type='hidden' name='value' id='hiddenValue' value='$x'>
-        <button id='submit' type='submit' name='submitUserName' class='$x'>$x</button>
+        <button id='submit' type='submit' name='submitUserName' class='$x'>
+        <span class='material-symbols-outlined'>person</span>
+        $x</button>
       </form>
     </div>";
       // echo "<script>let chatInstance = document.querySelector(".$x");</script>";
@@ -218,11 +175,12 @@ function test_input($data) {
     
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-      $_SESSION['Flag']='False';
+      // $_SESSION['Flag']='False';
 
       if (isset($_POST['submitUserName'])) {
        
         $_SESSION['sellerUser'] = $_POST['value']; // Store the value in session
+        $sellerUserGlobal=$_SESSION['sellerUser'];
         listTexts($_SESSION['sellerUser']);
         $_SESSION['Flag']='True';
         // echo $_SESSION['Flag'];
@@ -234,90 +192,26 @@ function test_input($data) {
 
       $chatValue = test_input($_POST["chatValue"]);
 
-    
-        // echo $_SESSION['Flag'];
-        if($_SESSION['Flag']=='True'){
-         send($chatValue, $_SESSION['sellerUser']); // Access the value from session
-         listTexts($_SESSION['sellerUser']);
+        send($chatValue, $sellerUserGlobal); // Access the value from session
+         listTexts($sellerUserGlobal);
+  //  sendDefault($chatValue); // Access the value from session
+  //         defaultText();    
+  //       // echo $_SESSION['Flag'];
+        // if($_SESSION['Flag']=='True'){
+        //  send($chatValue, $sellerUserGlobal); // Access the value from session
+        //  listTexts($_SESSION['sellerUser']);
   
-        }
-        else {
-          sendDefault($chatValue); // Access the value from session
-          defaultText();
+        // }
+        // else {
+        //   sendDefault($chatValue); // Access the value from session
+        //   defaultText();
 
-        }
-          
-      // header("Refresh:5");
-
+        // }
     }
   
   } else{
     defaultText();
   }
-    
-    
-
-    // function listTexts($sellerUser,$buyerUser){
-
-    //   global $db;
-    //   $sql2 = "SELECT sellerConvo,buyerConvo FROM chat where BuyerUserName='$buyerUser' and SellerUserName='$sellerUser' ORDER by chatTime";
-    //   $res2=$db->query($sql2);  
-    //   if($res2->num_rows>0)
-    //   {
-  
-    //     while($row=$res2->fetch_assoc())
-    //     {
-    //       echo "<br>";
-    //       if($row['sellerConvo']!=null){
-    //         // echo "<div class='sellerChat'>seller:  $row['sellerConvo']</div>";
-    //         echo "<div class='sellerChat'>seller: ".$row['sellerConvo']."</div>";
-
-
-    //         // echo "seller:  ".$row['sellerConvo'];
-    //         echo "<br>";
-    //       }
-    //       if($row['buyerConvo']!=null){
-    //         // echo "<div class='buyerChat'>buyer:  $row['buyerConvo']</div>";
-    //         echo "<div class='buyerChat'>buyer: ".$row['buyerConvo']."</div>";
-
-    //         // echo "buyer: ". $row['buyerConvo'];
-    //       echo "<br>";
-    //       }
-    //     }
-    //   }
-    // }
-    // $buyerUsername2=$_SESSION['BuyerUserName'] ;
-    
-    // $sellers=[];
-    // $sql2 = "SELECT sellerConvo,buyerConvo FROM chat where BuyerUserName='$buyerUsername2' and SellerUserName='$SellerUserName2' ORDER by chatTime";
-    
-
-    // $res2=$db->query($sql2);  
-    // if($res2->num_rows>0)
-    // {
-
-    //   while($row=$res2->fetch_assoc())
-    //   {
-    //     if($row['sellerConvo']!=null){
-    //       echo "seller: ".$row['sellerConvo'];
-    //       echo "<br>";
-    //     }
-    //     if($row['buyerConvo']!=null){
-    //       echo "buyer: ". $row['buyerConvo'];
-    //     echo "<br>";
-
-    //     }
-        
-    //     // array_push($sellers, $row['buyerConvo']);
-    
-    //   }
-    // }
-    
-   
-      // foreach ($sellers as $x) {
-      //   echo "<div>$x</div>";
-      // }
-     
       ?>
       <div class="textInput">
         <form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>">
@@ -325,7 +219,7 @@ function test_input($data) {
         <div class="input-box">
             <input type="text" name="chatValue" id="chatValue" required placeholder="insert text"> 
           </div>
-          <input type="submit" value="send" name="send"class="send-btn"> <br>
+          <input type="submit" value="send" name="send"class="send-btn">
           <input type="hidden" name="classNameInput" id="classNameInput" value="">
         </form>
       </div>
@@ -334,12 +228,8 @@ function test_input($data) {
 
 </body>
 <script > 
-
-
-
 let chatListElements = document.querySelectorAll('.chatListElement');
 let listOfChats = document.querySelector('.chatInstance');
-
 
 chatListElements.forEach((c)=>{
   
@@ -347,89 +237,10 @@ chatListElements.forEach((c)=>{
     document.getElementById("hiddenValue").value = c.id;
     alert(document.getElementById("hiddenValue").value);
     listOfChats.innerHTML='';
-  })
-})
+  });
+});
 
 
-// chatListElements.addEventListener('click',()=>{
-//   for (let i = 0; i < chatListElements.length; i++) {
-//     for(let i = 0; i <listOfChats.length; i++) {
-//       listOfChats[i].innerHTML = '';
-//     }
-//   }
-// })
-//   let elements = document.getElementsByClassName('chatList');
-//   console.log(elements);
-// let listOfChats = document.getElementsByClassName('chatInstance');
-// let myFunction = function() {
-  
-  
-//   for(let i = 0; i < listOfChats.length; i++) {
-//     listOfChats[i].innerHTML = '';
-//     // console.log(listOfChats[i]);
-//   }
- 
-
-// };
-
-// for (let i = 0; i < elements.length; i++) {
-//   let children = elements[i].children;
-
-//   for (let j = 0; j < children.length; j++) {
-//     let childClassName = children[j].className;
-
-//     children[j].addEventListener('click', );
-//   }
-// }
 
 </script>
 </html>
-
-
-<?php
-
-// if ($_SERVER["REQUEST_METHOD"] == "POST") {
-//   if (isset($_POST['submitUserName'])) {
-//     // echo"<script>let chatListElements = document.querySelectorAll('.chatListElement');
-//     // let listOfChats = document.querySelector('.chatInstance');
-    
-    
-//     // chatListElements.forEach((c)=>{
-      
-//     //   c.addEventListener('click',()=>{
-//     //     document.getElementById('hiddenValue').value = c.id;
-//     //     alert(document.getElementById('hiddenValue').value);
-//     //     listOfChats.innerHTML='';
-//     //   })
-//     // })</script>";
-//     $sellerUser = $_POST['value'];
-//     echo'value: '.$_POST['value'];
-//     $buyerUser=$_SESSION['BuyerUserName'] ;
-//     echo 'inside post sel: '.$sellerUser;
-//     echo "<br>";
-//     echo 'inside post buy: '.$buyerUser;
-//     // listTexts($sellerUser,$buyerUser);
-//     // $sql2 = "SELECT sellerConvo,buyerConvo FROM chat where BuyerUserName='$buyerUser' and SellerUserName='$sellerUser' ORDER by chatTime";
-    
-
-//     // $res2=$db->query($sql2);  
-//     // if($res2->num_rows>0)
-//     // {
-
-//     //   while($row=$res2->fetch_assoc())
-//     //   {
-//     //     echo "<br>";
-//     //     if($row['sellerConvo']!=null){
-//     //       echo "seller: ".$row['sellerConvo'];
-//     //       echo "<br>";
-//     //     }
-//     //     if($row['buyerConvo']!=null){
-//     //       echo "buyer: ". $row['buyerConvo'];
-//     //     echo "<br>";
-//     //     }
-//     //   }
-//     // }
-//   }
-// }
-
-?>
--- chat/SellerChat.php
+++ chat/SellerChat.php
@@ -11,14 +11,19 @@ $db=$Con->getConnection();
 
 $sellerUserGlobal=$_SESSION["SellerUserName"];
 
+
 function listTexts($buyerUser){
   global $sellerUserGlobal;
   global $db;
-  
-  echo "sel $sellerUserGlobal";
-  echo "<br>";
-  echo "buy $buyerUser";
-  echo "<br>";
+  echo '<div class="header">
+        <div class="receiverInfo">
+        <span class="material-symbols-outlined">account_circle</span>
+        <span class="receiverUserName">'.$buyerUser.'</span>
+        </div>
+        <span class="material-symbols-outlined">arrow_back</span>
+        </div>
+        <div class="chatBox">';
+
   $sql2 = "SELECT sellerConvo,buyerConvo FROM chat where BuyerUserName='$buyerUser' and SellerUserName='$sellerUserGlobal' ORDER by chatTime";
   $res2=$db->query($sql2);  
   if($res2->num_rows>0)
@@ -26,22 +31,15 @@ function listTexts($buyerUser){
 
     while($row=$res2->fetch_assoc())
     {
-      echo "<br>";
       if($row['sellerConvo']!=null){
-        echo "<div class='sellerChat'>seller: ".$row['sellerConvo']."</div>";
-
-
-        // echo "seller:  ".$row['sellerConvo'];
-        echo "<br>";
+        echo "<div class='senderChat'><span>".$row['sellerConvo']."</span></div>";
       }
       if($row['buyerConvo']!=null){
-        echo "<div class='buyerChat'>buyer: ".$row['buyerConvo']."</div>";
-
-        // echo "buyer: ". $row['buyerConvo'];
-      echo "<br>";
-      }
+        echo "<div class='receiverChat'><span>".$row['buyerConvo']."</span></div>";
+    }
     }
   }
+echo "</div>";
 }
 
 function send($chatValue,$buyerUser){
@@ -76,7 +74,12 @@ function test_input($data) {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="BuyerChat.css" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link href="https://fonts.googleapis.com/css2?family=Exo+2:ital,wght@0,100..900;1,100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+  <link rel="stylesheet" href="BuyerChat.css?v=<?php echo time(); ?>" />
  
   <title>SellerChat</title>
   
